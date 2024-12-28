b7f9463e8b077881844da02007da2741b3ce9b26
neerajSharma94 <neerajsharma@Neerajs-MacBook-Air.local>
2024-12-29 04:17:20 +0530

    url log added

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,11 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        const url = process.env.MONGO_URI;
+        if (!url) {
+            console.log("oops! url not found to connect with monogodb.");
+        }
+        await mongoose.connect(url);
         console.log("MongoDB connected successfully");
     } catch (error) {
         console.error("MongoDB connection error:", error);
